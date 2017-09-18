d414367475efb64002fe062be1b556cc5050062a
Dakota Nichols <dakotachantz@gmail.com>
2017-09-18 14:25:33 -0500

    app functional

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import Header from './Header';
+import Footer from './Footer';
+import BaseLayout from './BaseLayout';
+import ParentComponent from './ParentComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BaseLayout></BaseLayout>
+        <Header />
+        <ParentComponent />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
